b4d535d3ce1d1947f39358279afebd4351e6a756
Ribelo <r.krzywaznia@gmail.com>
2019-09-12 18:42:10 +0200

    org table creator

--- src/ribelo/torgau.clj
+++ src/ribelo/torgau.clj
@@ -28,23 +28,29 @@
 (defn ->active-datetime [datetime]
   (jt/format "'<'yyyy-MM-dd E HH:mm'>'" datetime))
 
-(defn org-table
+(defn table
   ([ks rows]
-     (when (seq rows)
-       (let [widths (map
-                     (fn [k]
-                       (apply max (count (str k)) (map #(count (str (get % k))) rows)))
-                     ks)
-             spacers (map #(apply str (repeat % "-")) widths)
-             fmts (map #(str "%" % "s") widths)
-             fmt-row (fn [leader divider trailer row]
-                       (str leader
-                            (apply str (interpose divider
-                                                  (for [[col fmt] (map vector (map #(get row %) ks) fmts)]
-                                                    (format fmt (str col)))))
-                            trailer))]
-         (println (fmt-row "| " " | " " |" (zipmap ks ks)))
-         (println (fmt-row "|-" "-+-" "-|" (zipmap ks spacers)))
-         (doseq [row rows]
-           (println (fmt-row "| " " | " " |" row))))))
-  ([rows] (org-table (keys (first rows)) rows)))
+   (when (seq rows)
+     (let [widths  (map
+                    (fn [k]
+                      (apply max (count (str k)) (map #(count (str (get % k))) rows)))
+                    ks)
+           align   (->> ks
+                        (map (fn [k]
+                               (if (every? number? (map #(get % k) rows)) :r :l))))
+           spacers (map #(apply str (repeat % "-")) widths)
+           fmts    (map #(case %2
+                           :r (str "%" %1 "s")
+                           :l (str "%-" %1 "s")) widths align)
+           fmt-row (fn [leader divider trailer row]
+                     (str leader
+                          (apply str (interpose divider
+                                                (for [[col fmt] (map vector (map #(get row %) ks) fmts)]
+                                                  (format fmt (str col)))))
+                          trailer \newline))]
+       (apply str
+              (into [(fmt-row "| " " | " " |" (zipmap ks ks))
+                     (fmt-row "|-" "-+-" "-|" (zipmap ks spacers))]
+                    (for [row rows]
+                      (fmt-row "| " " | " " |" row)))))))
+  ([rows] (table (keys (first rows)) rows)))
